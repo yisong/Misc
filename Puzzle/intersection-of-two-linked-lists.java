5e415d451b34ff1ef17740f3f74308ade37e73aa
Yisong Song <yisong@users.noreply.github.com>
2015-02-18 22:00:10 -0800

    Create intersection-of-two-linked-lists.java

---
+++ Puzzle/intersection-of-two-linked-lists.java
@@ -0,0 +1,70 @@
+/***
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+
+For example, the following two linked lists:
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗            
+B:     b1 → b2 → b3
+begin to intersect at node c1.
+
+Notes:
+
+If the two linked lists have no intersection at all, return null.
+The linked lists must retain their original structure after the function returns.
+You may assume there are no cycles anywhere in the entire linked structure.
+Your code should preferably run in O(n) time and use only O(1) memory.
+
+***/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lengthA = 0;
+        int lengthB = 0;
+        ListNode A = headA;
+        ListNode B = headB;
+        while (A != null) {
+            A = A.next;
+            lengthA ++;
+        }
+        while (B != null) {
+            B = B.next;
+            lengthB ++;
+        }
+        A = headA;
+        B = headB;
+        while (lengthA != lengthB) {
+            if (lengthA > lengthB) {
+                A = A.next;
+                lengthA--;
+            } else {
+                B = B.next;
+                lengthB--;
+            }
+        }
+        while (A != null && B != null) {
+            if (A == B) {
+                return A;
+            } else {
+                A = A.next;
+                B = B.next;
+            }
+        }
+        return null;
+    }
+}
